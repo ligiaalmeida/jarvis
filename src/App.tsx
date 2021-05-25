189f53234e14bcebe3a174726ad94b28a7ed1632
Silva, Ligia (154-Extern) <silligi@americas.corpdir.net>
2021-05-25 13:25:20 -0300

    feat: develop configs

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello Word from JARVIS!
-    </div>
-  );
+  return <div className="App">Hello Word from JARVIS!</div>;
 }
 
 export default App;
