b7570e682caf8ad6a6ecb6ebd94fdc00e7bd528b
Silva, Ligia (154-Extern) <silligi@americas.corpdir.net>
2021-05-25 14:07:45 -0300

    feat: migr src/pages/SignInPage folder

---
+++ src/pages/SignInPage/index.tsx
@@ -0,0 +1,173 @@
+import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { Container, Row } from 'components/Layout';
+import Ring from 'components/Icons/Loaders/Ring';
+
+import { SignInActions } from 'store/ducks/auth';
+import api from 'services/api';
+
+import Logo from 'assets/img/logo-jarvis.png';
+
+import texts from './texts';
+
+import * as S from './styles';
+import Footer from '../../components/Footer';
+import { Polices } from '../../types';
+
+const INITIAL_INPUT_STATE = { user: '', pwd: '' };
+
+const SignInPage = () => {
+  const [messageError, setMessageError] = useState('');
+  const [eachEntry, setEachEntry] = useState(INITIAL_INPUT_STATE);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const history = useHistory();
+
+  const { polices, isLogin } = SignInActions;
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEachEntry({ ...eachEntry, [e.target.name]: e.target.value });
+  };
+
+  let clearLoading: NodeJS.Timeout = (0 as unknown) as NodeJS.Timeout;
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const time = 800;
+
+    try {
+      setIsLoading(true);
+      const { data } = await api('/').post('jarvis/api/auth', eachEntry);
+
+      if (data.status_code === 200) {
+        if (typeof data.message !== 'string') {
+          clearLoading = setTimeout(() => {
+            dispatch(polices(data.message.auth));
+            dispatch(isLogin(true));
+            history.push(`/${(data.message.auth[0] as Polices).menu_item[0].name}`); // envia o user pra linha vinda do back
+          }, time);
+        } else {
+          clearLoading = setTimeout(() => {
+            setMessageError(data.message);
+            setIsLoading(false);
+          }, time);
+        }
+      }
+
+      if (data.status_code > 200) {
+        clearLoading = setTimeout(() => {
+          setMessageError(data.message);
+          setIsLoading(false);
+        }, time);
+      }
+    } catch (err) {
+      console.log('Erro:', err);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(clearLoading);
+    };
+  });
+
+  return (
+    <S.ContainerWrapper>
+      <Row className="sign-in__row-container">
+        <Container xs={9} lg={10} className="sign-in__img-hero" />
+
+        <Container xs={7} lg={6} className="sign-in__form">
+          <Row flexDirection="column" className="sign-in__row-content">
+            <Container padding={{ xs: [0, 0, 0, 0, 'px'] }} className="sign-in__content">
+              <img src={Logo} alt="Texto com a marca MB-Jarvis" onDragStart={(e) => e.preventDefault()} />
+              <S.BlueBox />
+              <S.FormContent>
+                <motion.div
+                  initial={{ opacity: 0, y: 15 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.6, delay: 0.1 }}
+                >
+                  <Container xs={13} lg={10} className="sign-in__form-content">
+                    <h1>{texts.title.pt_br}</h1>
+                    <p>{texts.description.pt_br}</p>
+
+                    <form onSubmit={handleSubmit}>
+                      <Container
+                        padding={{ xs: [0, 0, 0, 0, 'px'] }}
+                        flexDirection="column"
+                        className="sign-in__form-group"
+                        xs={16}
+                      >
+                        <label htmlFor="user">{texts.form.user.label.pt_br}</label>
+                        <input
+                          id="user"
+                          type="text"
+                          name="user"
+                          value={eachEntry.user}
+                          placeholder={texts.form.user.placeholder.pt_br}
+                          onChange={(e) => {
+                            if (messageError) setMessageError('');
+                            handleInputChange(e);
+                          }}
+                        />
+                      </Container>
+
+                      <Container flexDirection="column" className="sign-in__form-group" xs={16}>
+                        <label htmlFor="password">{texts.form.password.name.pt_br}</label>
+                        <input
+                          type="password"
+                          id="password"
+                          name="pwd"
+                          value={eachEntry.pwd}
+                          placeholder={texts.form.password.placeholder.pt_br}
+                          onChange={(e) => {
+                            if (messageError) setMessageError('');
+                            handleInputChange(e);
+                          }}
+                        />
+                      </Container>
+
+                      <AnimatePresence>
+                        {messageError && (
+                          <motion.div
+                            initial={{ y: 10, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            exit={{ y: 10, opacity: 0 }}
+                            transition={{ damping: 300, duration: 0.4 }}
+                          >
+                            <Container
+                              flexDirection="column"
+                              className="sign-in__form-group sign-in__form-group--message"
+                              xs={16}
+                              padding={{ xs: [0, 0, 0, 0, 'px'] }}
+                            >
+                              <span>{messageError}</span>
+                            </Container>
+                          </motion.div>
+                        )}
+                      </AnimatePresence>
+
+                      <button type="submit">
+                        {!isLoading && texts.form.button.pt_br}
+                        {isLoading && <Ring width={32} borderWidth={3} />}
+                      </button>
+                    </form>
+                  </Container>
+                </motion.div>
+              </S.FormContent>
+            </Container>
+          </Row>
+          <Footer />
+        </Container>
+      </Row>
+    </S.ContainerWrapper>
+  );
+};
+
+export default SignInPage;
---
+++ src/pages/SignInPage/styles.ts
@@ -0,0 +1,210 @@
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+import { Elevation, Transition } from 'utils/styles/mixins';
+import HeroImg from 'assets/img/login__hero--2.jpg';
+
+export const ContainerWrapper = styled.div`
+  ${(props) => {
+    const { theme } = props;
+
+    return css`
+      position: relative;
+      background: ${theme.colors.white};
+
+      .sign-in__row-container {
+        position: relative;
+        width: 100%;
+        height: 100vh;
+      }
+
+      .sign-in__img-hero {
+        position: relative;
+        overflow: hidden;
+        background-size: cover;
+        background-image: url(${HeroImg});
+        background-position: 30% 10%;
+
+        ${theme.breakpoints.default('min').xs(css`
+          display: none;
+        `)};
+
+        ${theme.breakpoints.default('min').md(css`
+          display: block;
+        `)};
+      }
+
+      .sign-in__row-content {
+        position: absolute;
+        width: 100%;
+        top: calc(50% + 7.5rem);
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+      }
+
+      .sign-in__content {
+        img {
+          width: 30rem;
+          margin: 0 auto 2rem;
+        }
+      }
+
+      footer {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+      }
+    `;
+  }}
+`;
+
+export const Hero = styled.img`
+  ${(props) => {
+    const { theme } = props;
+
+    return css`
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-55%, -50%);
+
+      ${theme.breakpoints.default('min').xs(css`
+        width: auto;
+        height: 105%;
+        display: none;
+      `)};
+
+      ${theme.breakpoints.default('min').sm(css`
+        display: block;
+      `)};
+
+      ${theme.breakpoints.default('min').md(css`
+        width: auto;
+        height: 105%;
+      `)};
+
+      ${theme.breakpoints.default('min').lg(css`
+        width: auto;
+      `)};
+    `;
+  }};
+`;
+
+export const FormContent = styled.div`
+  ${(props) => {
+    const { theme } = props;
+
+    return css`
+      position: relative;
+      top: -14rem;
+      width: 100%;
+
+      .sign-in__form-content {
+        position: relative;
+        margin: 0 auto;
+        padding: 4rem;
+        border-radius: ${theme.unit}px;
+        background: ${theme.colors.white};
+        ${Elevation(2)};
+
+        h1 {
+          font-size: 2.8rem;
+          color: ${theme.colors.primary_1};
+        }
+
+        p {
+          font-size: 1.8rem;
+          color: ${theme.colors.grey_1};
+          margin-bottom: 4rem;
+        }
+      }
+
+      .sign-in__form-group {
+        width: 100%;
+        margin-bottom: 1.5rem;
+
+        &--message {
+          margin-top: ${theme.unit * 3}px;
+          margin-bottom: 0;
+          padding: 1.5rem 1rem;
+          border-radius: 4px;
+          border: 1px solid ${theme.colors.grey_4};
+          background-color: ${theme.colors.grey_8};
+
+          span {
+            font-family: ${theme.typography.family.title};
+            font-size: ${theme.typography.size.normal};
+            color: ${theme.colors.primary_1};
+          }
+        }
+
+        + div {
+          padding-left: 0;
+          margin-bottom: 0;
+        }
+
+        label {
+          color: ${theme.colors.grey_2};
+          font-size: ${theme.typography.size.normal};
+          text-align: left;
+        }
+
+        input {
+          font-size: ${theme.typography.size.normal};
+          padding: 8px 16px;
+          border-radius: 4px;
+          border: 1px solid ${theme.colors.grey_3};
+
+          &:focus {
+            outline: 0;
+            border: 1px solid ${theme.colors.primary_3};
+          }
+        }
+      }
+
+      button {
+        border: 0;
+        border-radius: 4px;
+        margin-top: 4rem;
+        font-family: 'DaimlerLight', sans-serif;
+        color: ${theme.colors.white};
+        font-size: ${theme.typography.size.normal};
+        background-color: ${theme.colors.primary_1};
+        ${Transition('background-color', 0.3)};
+        cursor: pointer;
+
+        :hover {
+          background-color: ${shade(0.2, theme.colors.primary_1)};
+        }
+
+        ${theme.breakpoints.default('min').xs(css`
+          padding: 1.5rem 7rem;
+        `)};
+
+        ${theme.breakpoints.default('min').md(css`
+          padding: 1rem 3rem;
+        `)};
+
+        ${theme.breakpoints.default('min').lg(css`
+          padding: 1.5rem 7rem;
+        `)};
+      }
+    `;
+  }}
+`;
+
+export const BlueBox = styled.div`
+  position: relative;
+
+  ${(props) => {
+    const { theme } = props;
+
+    return css`
+      padding: 10rem 0;
+      color: ${theme.colors.white};
+      background: ${theme.colors.primary_1};
+    `;
+  }}
+`;
