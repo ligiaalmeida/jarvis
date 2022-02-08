9f4caf80af6cfb23bc28ea6635d60dd2188e4ef4
Mendes, Camila (154-Extern) <cammend@americas.corpdir.net>
2022-02-08 15:27:37 -0300

    [simplified view]

--- src/pages/FaultPredictionPage/index.tsx
+++ src/pages/FaultPredictionPage/index.tsx
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { theme } from 'styles/theme';
 import { useWindowWidth, useSocket } from 'hooks';
-import { FaultPredictionActions } from 'store/ducks/faultPrediction';
 import env from 'constants/env';
 import namespace from 'constants/namespace';
 import payload from 'constants/payload';
 
 import SimplifiedView from 'components/ViewMode/SimplifiedView';
-import DetailedView from 'components/ViewMode/DetailedView';
-import ViewingModeSetting from 'components/ViewingModeSetting';
 import InputList from 'components/Pages/InputList';
 import Footer from 'components/Footer';
 
-import { StateMapToPropsGlobal, ModeView, Pathname, RouterProps } from 'types';
+import { StateMapToPropsGlobal, Pathname, RouterProps } from 'types';
 
 import CurrentFaultsLoader from './CurrentFaultsLoader';
 import * as S from './styles';
@@ -22,17 +19,11 @@ import * as S from './styles';
 const FaultPredictionPage = () => {
   const [heightNavigation, setHeightNavigation] = useState(0);
 
-  const settingsPage = useSelector(
-    (state: Pick<StateMapToPropsGlobal, 'faultPredictionPage'>) =>
-      state.faultPredictionPage
-  );
   const settingsGlobal = useSelector(
     (state: Pick<StateMapToPropsGlobal, 'global'>) => state.global
   );
   const router = useSelector((state: RouterProps) => state.router);
 
-  const { toggleModeView } = FaultPredictionActions;
-  const dispatch = useDispatch();
   const width = useWindowWidth();
 
   const URI_BASE =
@@ -54,47 +45,23 @@ const FaultPredictionPage = () => {
   }
 
   useEffect(() => {
-    if (settingsPage.modeView === 'detailed') {
-      document.body.classList.add('fault-prediction__bg--primary2');
-    }
-
-    return () => {
-      document.body.classList.remove('fault-prediction__bg--primary2');
-    };
-  });
-
-  useEffect(() => {
     const height = document.querySelector('#root > #navigation');
     setHeightNavigation(Math.floor(height?.clientHeight as number));
   }, [width, heightNavigation]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(toggleModeView(event.target.value as ModeView));
-  };
-
   return (
     <>
-      <S.Main modeViewType={settingsPage?.modeView}>
+      <S.Main>
         <InputList
           padding={`${theme.distance.normal}rem`}
           pathname={router.location.pathname as Pathname}
-        >
-          <ViewingModeSetting
-            pageName="faultPredictionPage"
-            toggleNavigation={settingsGlobal.toggleNavigation}
-            handleChange={handleChange}
-          />
-        </InputList>
-        {!data && (
+        />
+        {!data ? (
           <S.Loader>
             <CurrentFaultsLoader />
           </S.Loader>
-        )}
-        {data && settingsPage.modeView === 'simplified' && (
+        ) : (
           <SimplifiedView namespace="faultPredictionPage" message={data} />
-        )}
-        {data && settingsPage.modeView === 'detailed' && (
-          <DetailedView namespace="faultPredictionPage" message={data} />
         )}
       </S.Main>
       <Footer />
