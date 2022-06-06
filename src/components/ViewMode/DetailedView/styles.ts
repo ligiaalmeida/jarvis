4776f58db0d36da603a3b6d23bd7e53d19b21f8b
Silva, Ligia (154-Extern) <silligi@americas.corpdir.net>
2022-06-06 13:52:15 -0300

    Revert "Merge branch 'feat/Sprint-2/FaultPredictionPage' into 'develop-web'"
    This reverts commit b1f5bf7bcdfff2ba3af62a12342ad45269f7f6ee, reversing
    changes made to a429899a04e4c6b61615d997187615c05db90403.

--- src/components/ViewMode/DetailedView/styles.ts
+++ src/components/ViewMode/DetailedView/styles.ts
@@ -1,17 +1,34 @@
 import styled, { css } from 'styled-components';
 
 export const RowStations = styled.div`
-  position: relative;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  align-items: stretch;
+  ${() => {
+    // const { theme } = props;
 
-  & > div {
-    min-height: 26rem;
-    width: 100%;
-  }
+    return css`
+      position: relative;
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      align-items: stretch;
+
+      & > div {
+        min-height: 26rem;
+      }
+    `;
+  }};
+`;
+
+export const StationsContent = styled.div`
+  ${() => {
+    // const { theme } = props;
+
+    return css`
+      position: relative;
+      display: flex;
+      flex-direction: column;
+    `;
+  }};
 `;
 
 export const Container = styled.div`
@@ -23,7 +40,7 @@ export const Container = styled.div`
       display: flex;
       flex-direction: column;
       margin-top: 1rem;
-      padding: 0 0 ${theme.distance.normal}rem 0;
+      padding: 0 ${theme.distance.normal}rem ${theme.distance.normal}rem;
     `;
   }};
 `;
--- src/components/ViewMode/SimplifiedView/index.tsx
+++ src/components/ViewMode/SimplifiedView/index.tsx
@@ -3,17 +3,15 @@ import React from 'react';
 import { CurrentFaultsActions } from 'store/ducks/currentFaults';
 import { FaultPredictionActions } from 'store/ducks/faultPrediction';
 
-import CurrentFaultStation from 'components/StationItemFaults/CurrentFaultStation';
-import FaultPredictionStation from 'components/StationItemFaults/FaultPredictionStation';
+import StationItemFaults from 'components/StationItemFaults';
 import NavTabs from 'components/NavTabs';
 import Error from 'components/Icons/Error';
 import MessageError from 'components/Messages/Error';
 
 import {
-  CurrentFaultItem,
   CurrentFaultsPayload,
   FaultPredictionPayload,
-  StationItemCurrentFaultsProps,
+  StationItemFaultsProps,
 } from 'components/StationItemFaults/types';
 import { KeysOfPagesContainingStations } from 'types';
 import * as Types from '../types';
@@ -23,16 +21,28 @@ const SimplifiedView = ({
   isDrawerDetails = false,
   namespace,
 }: Types.SimplifiedViewProps) => {
+  let payload: CurrentFaultsPayload[] | FaultPredictionPayload[] | undefined;
   const data: Types.TabsData<React.ReactElement>[] = [];
 
+  switch (namespace) {
+    case 'faultPredictionPage':
+      payload = message
+        ? message.fault_prediction?.filter(
+            (station) => station.stop_fail_list.length > 0
+          )
+        : [];
+      break;
+    default:
+      payload = message
+        ? message?.current_faults?.filter(
+            (station) => station.fail_list.length > 0
+          )
+        : [];
+      break;
+  }
+
   if (namespace === 'currentFaultsPage') {
-    const stationList: CurrentFaultsPayload[] = message
-      ? (message as CurrentFaultsPayload[])?.filter(
-          (station: { fail_list: CurrentFaultItem[] }) =>
-            station.fail_list.length > 0
-        )
-      : [];
-    stationList.map((station, idx) => {
+    (payload as CurrentFaultsPayload[])?.map((station, idx) => {
       if (idx % 24 === 0) {
         data.push({
           label: { id: idx, title: station.label },
@@ -41,7 +51,7 @@ const SimplifiedView = ({
       }
 
       data[data.length - 1].componentChildren.push(
-        <CurrentFaultStation
+        <StationItemFaults
           data={station}
           key={station.label}
           id={station.label}
@@ -51,31 +61,32 @@ const SimplifiedView = ({
         />
       );
       data.map((row, idxRow) => (row.label.id = idxRow));
+
       return data;
     });
   }
 
   if (namespace === 'faultPredictionPage') {
-    (message as FaultPredictionPayload[])?.map((station, idx) => {
-      if (station.stop_fail_list.length > 0) {
-        if (idx % 24 === 0) {
-          data.push({
-            label: { id: idx, title: station.label },
-            componentChildren: [],
-          });
-        }
-
-        data[data.length - 1].componentChildren.push(
-          <FaultPredictionStation
-            data={station}
-            key={station.label}
-            id={station.label}
-            isOnClick={isDrawerDetails}
-            typeView="simplified"
-          />
-        );
-        data.map((row, idxRow) => (row.label.id = idxRow));
+    (payload as FaultPredictionPayload[])?.map((station, idx) => {
+      if (idx % 24 === 0) {
+        data.push({
+          label: { id: idx, title: station.label },
+          componentChildren: [],
+        });
       }
+
+      data[data.length - 1].componentChildren.push(
+        <StationItemFaults
+          data={station}
+          key={station.label}
+          id={station.label}
+          namespace={namespace}
+          isOnClick={isDrawerDetails}
+          typeView="simplified"
+        />
+      );
+      data.map((row, idxRow) => (row.label.id = idxRow));
+
       return data;
     });
   }
@@ -84,7 +95,7 @@ const SimplifiedView = ({
     item.label.title += ` ao ${
       (
         item.componentChildren[item.componentChildren.length - 1]
-          .props as StationItemCurrentFaultsProps
+          .props as StationItemFaultsProps
       ).data.label
     }`;
   });
@@ -119,7 +130,7 @@ const SimplifiedView = ({
       ) : (
         <MessageError
           isVisible
-          title="Erro ao tentar buscar os dados"
+          title="Error ao tentar buscar os dados"
           description="Linha sem registro de falhas"
           icon={<Error />}
         />
