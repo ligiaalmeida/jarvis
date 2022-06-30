fb628acf572d719c0f831868659e55cbcaef16a0
Silva, Ligia (154-Extern) <silligi@americas.corpdir.net>
2022-06-30 12:14:53 -0300

    fix: updating client rendering API

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import { history, configuredStore } from './store';
 
 import App from './pages/App';
 
 const [store, persistor] = configuredStore();
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App persistor={persistor} store={store} history={history} />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+root.render(<App persistor={persistor} store={store} history={history} />);
