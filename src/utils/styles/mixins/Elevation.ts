3fc2121754e97f968b8bcfe8329f35b6c9bda853
Silva, Ligia (154-Extern) <silligi@americas.corpdir.net>
2022-08-08 15:34:19 -0300

    chore: fixing declaration of box-shadow values

--- src/utils/styles/mixins/Elevation.ts
+++ src/utils/styles/mixins/Elevation.ts
@@ -4,23 +4,24 @@ export const Elevation = (level: number) => {
   switch (level) {
     case 1:
       return css`
-        box-shadow: 0 3px 7px 0 rgba(0, 0, 0, 0.52);
+        box-shadow: 0px 3px 7px 0px rgba(0, 0, 0, 0.52);
       `;
     case 2:
       return css`
-        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+        box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16),
+          0 3px 6px rgba(0, 0, 0, 0.23);
       `;
     case 3:
       return css`
-        box-shadow: 0 1px 6px rgba(0, 0, 0, 0.12);
+        box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.12);
       `;
     case 4:
       return css`
-        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.05);
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.05);
       `;
     default:
       return css`
-        box-shadow: inset 0 7px 9px -7px rgba(0, 0, 0, 0.7);
+        box-shadow: inset 0px 7px 9px -7px rgba(0, 0, 0, 0.7);
       `;
   }
 };
